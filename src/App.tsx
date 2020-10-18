3ffb81b579488cdd0113e8734c693d759283e7a1
Bharat Gupta <bharat.gupta@flipkart.com>
2020-10-18 21:25:00 +0530

    removed few not required files. code cleanup.

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,8 @@ import BrandDetail from './components/BrandDetail';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import VerifyAccount from './components/VerifyAccount';
-import SignUpReferralCode from './components/SignUpReferralCode';
 import * as StorageHelper from './helpers/StorageHelper';
 import authReducer, { authInitialState, AuthActions, AuthState } from './reducers/AuthReducer';
-import SocialSignIn from './components/SocialSignIn';
 import Rewards from './components/Rewards';
 import SatsSpin from './components/SatsSpin';
 import AccountLogin from './components/AccountLogin';
@@ -114,13 +112,9 @@ function App() {
 								screenOptions={{ header: () => null }}
 							>
 								<React.Fragment>
-									<Stack.Screen name="SignUp" component={SignUp} />
-									<Stack.Screen name="SignIn" component={SignIn} />
-									<Stack.Screen name="CreateAccount" component={CreateAccount} />
 									<Stack.Screen name="AccountLogin" component={AccountLogin} />
-									<Stack.Screen name="SocialSignIn" component={SocialSignIn} />
+									<Stack.Screen name="CreateAccount" component={CreateAccount} />
 									<Stack.Screen name="VerifyAccount" component={VerifyAccount} />
-									<Stack.Screen name="SignUpReferralCode" component={SignUpReferralCode} />
 								</React.Fragment>
 						</Stack.Navigator>
 					)}
